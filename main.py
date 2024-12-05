b5196df5b01c92a55ad2974bfd57f6400fdd363d
Ivan Bolorino <ivan.bolorino@gmail.com>
2024-12-05 17:02:56 -0300

    2

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from services import Operation
 import json
+import sys
 
 
 def main():
-    operations = []
-    operation = input()
-    print("##################")
-    print(operation)
-    print("##################")
-    while operation:
-        operations.append(json.loads(operation))
-        operation = input()
+    operations = [line.strip() for line in sys.stdin.readlines()]
+    operations = [json.loads(operation) for operation in operations if operation != '']
+    
     for orders in operations:
         op = Operation(orders)
         print(op.taxes)
@@ -19,4 +15,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
--- tests/test_main.py
+++ tests/test_main.py
@@ -7,16 +7,20 @@ from fixtures import set_case_tests_as_str
 
 class TestMain(unittest.TestCase):
     
-    @patch("builtins.input")
+    @patch("sys.stdin", new_callable=StringIO)
     @patch("sys.stdout", new_callable=StringIO)
     def test_main(self, mock_output, mock_input):
         case_tests = set_case_tests_as_str()
         for input_parameter, expected_value in case_tests:
             with self.subTest(input_data=input_parameter, expected_output=expected_value):
-                mock_input.side_effect = [input_parameter, '']
+                mock_input.write(input_parameter)
+                mock_input.seek(0)
+                
                 main()
                 output = mock_output.getvalue()
                 self.assertEqual(output, expected_value)
                 
+                mock_input.truncate(0)
+                mock_input.seek(0)
                 mock_output.truncate(0)
                 mock_output.seek(0)
